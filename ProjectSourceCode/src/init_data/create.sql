1eee99fe1ef8edb89c29bff35ea9a8a7fc1c62a5
Bhargav <bhsa1818@colorado.edu>
2025-11-06 10:59:23 -0700

    update sql

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,5 +1,8 @@
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     password VARCHAR(60) NOT NULL,
-    
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    dateOfBirth DATE
 );
