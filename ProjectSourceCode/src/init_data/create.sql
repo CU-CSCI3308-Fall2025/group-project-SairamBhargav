905438b30f0dfa4d2e138748702dd547eef81862
Aswaanth Karuppasamy <aska3174@colorado.edu>
2025-12-04 07:58:31 -0700

    register page

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -18,6 +18,11 @@ CREATE TABLE users (
     lastName VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     dateOfBirth DATE
+
+      -- NEW: user favorites for profile
+    favorite_genre   VARCHAR(100),
+    favorite_actor   VARCHAR(100),
+    favorite_actress VARCHAR(100)
 );
 
 -- User profiles (FIXED: references username now)
